788df80ed8d7240e7a293fee8717e3a1ed90f89d
miguelScrummers <miguel.gamboa@scrummers.co>
2020-04-03 11:16:56 -0500

    plate recognizer

--- licenseplatereader/src/main/java/co/scrummers/loteye/licenseplatereader/Reader.java
+++ licenseplatereader/src/main/java/co/scrummers/loteye/licenseplatereader/Reader.java
@@ -1,30 +1,69 @@
 package co.scrummers.loteye.licenseplatereader;
 
-import java.util.concurrent.TimeUnit;
-
+import android.content.Context;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import co.scrummers.loteye.licenseplatereader.interfaces.ReaderListener;
 import co.scrummers.loteye.licenseplatereader.jni.DecoderWrapper;
+import co.scrummers.loteye.licenseplatereader.util.MultipartUtility;
 
-public class Reader implements Runnable {
+public class Reader {
+    private final String token;
+    private final String url;
+    DecoderWrapper wrapper = null;
 
-    String path;
-    DecoderWrapper wrapper ;
-    public void read(String path){
-        this.path = path;
-        wrapper = new DecoderWrapper();
-        this.run();
+    public Reader(Context
+                          context) {
+        this.token = context.getString(R.string.PLATE_RECOGNIZER_TOKEN);
+        this.url = context.getString(R.string.PLATE_RECOGNIZER_URL);
+        this.wrapper = new DecoderWrapper();
     }
+    public void setListener(ReaderListener listener){
+        if(this.wrapper != null){
+            this.wrapper.setListener(listener);
+        }
+    }
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
 
-    @Override
-    public void run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
-        System.out.println("decoding");
-        wrapper.decodeImage(this.path);
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
 
-        /*try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-        System.out.println("finished");
+        return result.toString();
+    }
+    public void read(final String path){
+
+        final HashMap<String,String> headers = new HashMap<String,String>();
+        headers.put("Authorization","Token "+this.token);
+        final String url = this.url;
+        new Thread(new Runnable() {
+            public void run(){
+                //wrapper.decodeImage(path);
+                MultipartUtility multipart = null;
+                try {
+                    multipart = new MultipartUtility(url, "utf-8",headers);
+                    multipart.addFormField("regions","us");
+                    multipart.addFilePart("upload", new File(path));
+                    String response = multipart.finish();
+                    if(Reader.this.wrapper != null){
+                        Reader.this.wrapper._decodeReady(response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- licenseplatereader/src/main/java/co/scrummers/loteye/licenseplatereader/jni/DecoderWrapper.java
+++ licenseplatereader/src/main/java/co/scrummers/loteye/licenseplatereader/jni/DecoderWrapper.java
@@ -4,14 +4,19 @@ import co.scrummers.loteye.licenseplatereader.interfaces.ReaderListener;
 
 public class DecoderWrapper {
 
+    ReaderListener listener = null;
     static {
         System.loadLibrary("decoder");
     }
 
     public native void decodeImage(String path);
-
-    void _decodeReady(String result){
-        System.out.println(result);
+    public void setListener(ReaderListener listener){
+        this.listener = listener;
+    }
+    public void _decodeReady(String result){
+        if(this.listener != null){
+            this.listener.onResult(result);
+        }
     }
 }
 
